f0a49cc9dd2c48bb215cddb6f032e0c68b38c9d5
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-10-11 15:05:28 -0300

    Capitulo 22.22

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -9,6 +9,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -34,6 +36,8 @@ public class LancamentoService {
 	
 	private static final String DESTINATARIOS = "ROLE_PESQUISAR_LANCAMENTO";
 	
+	private static final Logger logger = LoggerFactory.getLogger(LancamentoService.class);
+	
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
@@ -56,13 +60,28 @@ public class LancamentoService {
 	//@Scheduled(cron = "0 58 11 * * *")
 	@Scheduled(fixedDelay = 1000 * 10)
 	public void avisarSobreLancamentosVencidos() {
+		if(logger.isDebugEnabled()) {
+			logger.debug("Preparando envio de emails de aviso de lancamento vencidos");
+		}
 		List<Lancamento> vencidos = lancamentoRepository
 			.findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate.now());
+		if(vencidos.isEmpty()) {
+			logger.info("Sem lancamentos vencidos para aviso");
+			return;
+		}
+		
+		logger.info("Existem {} lancamentos vencidos.", vencidos.size());
 		
 		List<Usuario> destinatarios = usuarioRepository
 				.findByPermissoesDescricao(DESTINATARIOS);
 		
+		if(destinatarios.isEmpty()) {
+			logger.warn("Existem lancamentos vencidos, mas o sistema não encontrou destinatarios");
+		}
+		
 		mailer.avisarSobreLancamentosVencidos(vencidos, destinatarios);
+		
+		logger.info("Envio de email de aviso  concluido");
 	}
 	
 	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
